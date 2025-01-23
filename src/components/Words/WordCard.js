115e81e01b816609fbcf20f39d5365fa43bf975f
LoraKamenova <cruzeiro@abv.bg>
2025-01-23 22:56:28 +0200

    word card change

--- src/components/Words/WordCard.js
+++ src/components/Words/WordCard.js
@@ -4,9 +4,9 @@ const WordCard = (props) => {
 
     return (
         <article className="word-card">
+            <p className="word-card-comment">{props.comment}</p>
             <p className="word-card-word"><span className='word-card-word-span'>{props.word}</span></p>
             <p className="word-card-description">{props.description}</p>
-            <p className="word-card-comment">{props.comment}</p>
         </article>
     );
 };
