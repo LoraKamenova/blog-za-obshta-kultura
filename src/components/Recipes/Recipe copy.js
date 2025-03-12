7d08a69c26ef95425d2c6a882463b0527bc826a0
LoraKamenova <cruzeiro@abv.bg>
2025-03-13 00:24:09 +0200

    migration to json

---
+++ src/components/Recipes/Recipe copy.js
@@ -0,0 +1,39 @@
+import { React, Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import '../../SharedCSS/SinglePost.css';
+import { Link } from "react-router-dom";
+import data from '../../assets/data.json';
+import rehypeRaw from 'rehype-raw';
+
+class Recipe extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { terms: null }
+  }
+
+async componentWillMount() {
+    const file = await import(`../../assets/posts/${this.props.id}.md`);
+    const response = await fetch(file.default);
+    const text = await response.text();
+    this.setState({ terms: text })
+  }
+
+  render() {
+
+    let obj = data.find(({_id}) => _id == this.props.id);
+
+    return (
+      <div className='post-page'>
+        <div className="post">
+          <ReactMarkdown className="post-markdown" children={this.state.terms} rehypePlugins={[rehypeRaw]} />
+        </div>
+        <div className='tag-container'>
+          <span className='tag-span'>Етикети:</span>
+          {obj.tags.map((x) => (<Link className='tag-link' to={`/recipes/tags`} state={{tag: `${x}`}}>{x}</Link>))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Recipe;
--- src/components/Recipes/Recipe.js
+++ src/components/Recipes/Recipe.js
@@ -1,32 +1,55 @@
 import { React, Component } from 'react';
-import ReactMarkdown from 'react-markdown';
-import '../../SharedCSS/SinglePost.css';
+import '../Recipes/Recipe.css';
 import { Link } from "react-router-dom";
 import data from '../../assets/data.json';
-import rehypeRaw from 'rehype-raw';
 
 class Recipe extends Component {
   constructor(props) {
     super(props)
-    this.state = { terms: null }
-  }
-
-async componentWillMount() {
-    const file = await import(`../../assets/posts/${this.props.id}.md`);
-    const response = await fetch(file.default);
-    const text = await response.text();
-    this.setState({ terms: text })
   }
 
   render() {
-
+    var post = require(`../../assets/posts/${this.props.id}.json`);
     let obj = data.find(({_id}) => _id == this.props.id);
 
     return (
-      <div className='post-page'>
-        <div className="post">
-          <ReactMarkdown className="post-markdown" children={this.state.terms} rehypePlugins={[rehypeRaw]} />
+      <div className='recipe-page'>
+        <div className="recipe">
+          <h1 className='recipe-heading'>{post.title}</h1>
+          <p className='recipe-paragraph'>{post.introduction}</p>
+          <div className='recipe-image-and-ingredientes'>
+            <div className='recipe-image-container'>
+            {(() => {
+              if (post.url[0] === "portrait") {
+                return (
+                  <img className='recipe-image portrait' src={post.url[1]}></img>
+                )
+            } else if (post.url[0] === "landscape") {
+              return (
+                <img className='recipe-image landscape' src={post.url[1]}></img>
+              )
+            }})()}
+            </div>
+            <div className='recipe-ingredients-container'>
+              <h3>Необходими продукти:</h3>
+              <ul className='recipe-ul'>{post.ingredients.map((ingredient => (<li className='recipe-li'>{ingredient}</li>)))}</ul>
+            </div>
+          </div>
+          <div className='recipe-steps-container container'>
+            <h3>Начин на приготвяне:</h3>
+            <ul className='recipe-ul'>{post.steps.map((step => (<li className='recipe-li'>{step}</li>)))}</ul>
+          </div>
+          <div className='recipe-notes-container container'>
+            <h3>Бележки:</h3>
+            <ul className='recipe-ul'>{post.notes.map((step => (<li className='recipe-li'>{step}</li>)))}</ul>
+          </div>
         </div>
+        {(() => {
+              if (post.secondPicture !== "") {
+                return (
+                  <img className='recipe-image landscape' src={post.secondPicture}></img>
+                )
+            }})()}
         <div className='tag-container'>
           <span className='tag-span'>Етикети:</span>
           {obj.tags.map((x) => (<Link className='tag-link' to={`/recipes/tags`} state={{tag: `${x}`}}>{x}</Link>))}
