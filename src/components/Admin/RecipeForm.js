6d67b83f327d8901e27c7118ee6ba393041af72c
LoraKamenova <cruzeiro@abv.bg>
2025-03-17 14:00:24 +0200

    Finished recipe admin form

--- src/components/Admin/RecipeForm.js
+++ src/components/Admin/RecipeForm.js
@@ -1,18 +1,29 @@
 import { React, useState } from 'react';
+import data from '../../assets/data.json';
 import './Forms.css';
 
 const RecipeForm = (props) => { 
 
+    let count = data.length;
+
     const [text, setText]=useState("");
+    const [text2, setText2]=useState("");
+    const [selectedOption, setSelectedOption] = useState("Салати");
+
     const [formData, setFormData] = useState({
         title: "",
         introduction: "",
         url: "",
+        url2: "", 
         ingredients: "",
         steps: "",
         notes: "",
-        url2: ""
+        tags: ""
     });
+
+    const  handleDropdownChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
 
     const handleChange = (event) => {
       const { name, value } = event.target;
@@ -23,14 +34,15 @@ const RecipeForm = (props) => {
         event.preventDefault();
 
         var jsonObject = {};
+        var jsonObjectShort = {};
 
-        let ingredients = formData.ingredients.split(";").map(s => s.trim());
+        let ingredients = formData.ingredients.split('\n').map(s => s.trim());
         ingredients = ingredients.filter(item => item);
 
-        let steps = formData.steps.split(";").map(s => s.trim());
+        let steps = formData.steps.split('\n').map(s => s.trim());
         steps = steps.filter(item => item);
 
-        let notes = formData.notes.split(";").map(s => s.trim());
+        let notes = formData.notes.split('\n').map(s => s.trim());
         notes = notes.filter(item => item);
 
         jsonObject.title = formData.title;
@@ -41,8 +53,27 @@ const RecipeForm = (props) => {
         jsonObject.notes = notes;
         jsonObject.url2 = formData.url2;
 
+        let tags = formData.tags.split(",").map(s => s.trim());
+        tags = tags.filter(item => item);
+
+        const current = new Date();
+        const day = ("0" + current.getDate()).slice(-2);
+        const month = ("0" + (current.getMonth() + 1)).slice(-2);
+        const year = current.getFullYear();
+        const date = `${day}/${month}/${year}`;
+
+        jsonObjectShort._id = count + 1;
+        jsonObjectShort.title = formData.title;
+        jsonObjectShort.date = date;
+        jsonObjectShort.category = "Рецепти";
+        jsonObjectShort.subcategory = selectedOption;
+        jsonObjectShort.tags = tags;
+        jsonObjectShort.url = formData.url;
+        jsonObjectShort.content = formData.introduction;
+
         // alert(`${JSON.stringify(jsonObject, null, 4)}`);
         setText(`${JSON.stringify(jsonObject, null, 4)}`);
+        setText2(`${JSON.stringify(jsonObjectShort, null, 4)}`);
     };
 
     function reset(event) {
@@ -54,17 +85,33 @@ const RecipeForm = (props) => {
             ingredients: "",
             steps: "",
             notes: "",
-            url2: ""
+            url2: "",
+            tags: ""
         });
+
         setText("");
+        setText2("");
     };
   
     return (
         <div className='form-page'>
             <form className='form' onSubmit={handleSubmit}>
-                <h1 className='form-heading'>Нова рецепта</h1>
+                
                 <div className='fields-container'>
                     <div className='form-half'>
+                    <h1 className='form-heading'>Нова рецепта</h1>
+                        <div className='input select'>
+                            <label className='select-label'>Подкатегория</label>
+                            <select className='select-option' value={selectedOption} onChange={handleDropdownChange}>
+                                <option  value="Салати">Салати</option>
+                                <option  value="Предястия">Предястия</option>
+                                <option  value="Основни">Основни</option>
+                                <option  value="Тестени">Тестени</option>
+                                <option  value="Десерти">Десерти</option>
+                                <option  value="Напитки">Напитки</option>
+                            </select> 
+                        </div>
+
                         {/* <label>Заглавие</label> */}
                         <input className='input'
                             type="text" 
@@ -80,7 +127,7 @@ const RecipeForm = (props) => {
                             id="introduction" 
                             name="introduction" 
                             placeholder="Представяне"
-                            rows="5"
+                            rows="3"
                             value={formData.introduction} 
                             onChange={handleChange}
                         />
@@ -94,22 +141,13 @@ const RecipeForm = (props) => {
                             value={formData.url} 
                             onChange={handleChange}
                         />
-                        {/* <label>Снимка 2</label> */}
-                            <input className='input'
-                            type="text" 
-                            id="url2" 
-                            name="url2" 
-                            placeholder="Допълнителна снимка"
-                            value={formData.url2} 
-                            onChange={handleChange}
-                        />
 
                         {/* <label>Необходими продукти</label> */}
                             <textarea className='input textarea'
                             id="ingredients" 
                             name="ingredients" 
                             placeholder="Необходими продукти"
-                            rows="12"
+                            rows="10"
                             value={formData.ingredients} 
                             onChange={handleChange}
                         />
@@ -121,7 +159,7 @@ const RecipeForm = (props) => {
                             type="text" 
                             id="steps" 
                             name="steps" 
-                            rows="15"
+                            rows="11"
                             placeholder="Стъпки"
                             value={formData.steps} 
                             onChange={handleChange}
@@ -132,9 +170,40 @@ const RecipeForm = (props) => {
                             type="text" 
                             id="notes" 
                             name="notes" 
-                            rows="7"
+                            rows="6"
                             placeholder="Бележки"
                             value={formData.notes} 
+                            onChange={handleChange}
+                        />
+
+                            {/* <label>Тагове</label>
+                            <textarea className='input textarea'
+                            type="text" 
+                            id="tags" 
+                            name="tags" 
+                            rows="1"
+                            placeholder="Етикети"
+                            value={formData.tags} 
+                            onChange={handleChange}
+                        /> */}
+
+                        {/* <label>Тагове 2</label> */}
+                        <input className='input'
+                            type="text" 
+                            id="tags" 
+                            name="tags" 
+                            placeholder="Етикети"
+                            value={formData.tags} 
+                            onChange={handleChange}
+                        />
+
+                        {/* <label>Снимка 2</label> */}
+                        <input className='input'
+                            type="text" 
+                            id="url2" 
+                            name="url2" 
+                            placeholder="Допълнителна снимка"
+                            value={formData.url2} 
                             onChange={handleChange}
                         />
 
@@ -147,8 +216,9 @@ const RecipeForm = (props) => {
                 </div>
             </form>
             <section className='result-section'>
-                <h1 className='result-heading'>JSON</h1>
-                <pre className='result-text'>{text}</pre>
+                {/* <h1 className='result-heading'>JSON</h1> */}
+                <pre className='result-text first'>{text}</pre>
+                <pre className='result-text second'>{text2}</pre>
             </section>
         </div>
     )
