1f6e2709857f2506d577f23c507ea6816c72994f
LoraKamenova <cruzeiro@abv.bg>
2025-03-21 23:17:06 +0200

    article page refactored

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,35 +1,35 @@
 import { React, Component } from 'react';
-import ReactMarkdown from 'react-markdown';
-import '../../SharedCSS/SinglePost.css';
+import '../Articles/Article.css';
 import { Link } from "react-router-dom";
 import data from '../../assets/data.json';
-import rehypeRaw from 'rehype-raw';
+import Sections from './Sections';
 
 class Article extends Component {
   constructor(props) {
     super(props)
-    this.state = { terms: null }
-  }
-
-async componentWillMount() {
-    const file = await import(`../../assets/posts/${this.props.id}.md`);
-    const response = await fetch(file.default);
-    const text = await response.text();
-    this.setState({ terms: text })
   }
 
   render() {
-
+    var post = require(`../../assets/posts/${this.props.id}.json`);
     let obj = data.find(({_id}) => _id == this.props.id);
 
     return (
-      <div className='post-page'>
-        <div className="post">
-          <ReactMarkdown className="post-markdown" children={this.state.terms} rehypePlugins={[rehypeRaw]}/>
-        </div>
-        <div className='tag-container'>
-          <span className='tag-span'>етикети</span>
-          {obj.tags.map((x) => (<Link className='tag-link' to={`/articles/tags`} state={{tag: `${x}`}}>{x}</Link>))}
+      <div className='article-page'>
+        <div className="article">
+          <h1 className='article-heading'>{post.title}</h1>
+          <p className='article-paragraph'>{post.introduction}</p>
+          <div>{post.sections.map(section => (<Sections data={section} />))}</div>
+          {(() => {
+              if (post.link !== "") {
+                return (
+                  <p>{post.link[0]}<a className='post-link' href={post.link[1]}>тук</a></p>
+                )
+          }})()}
+      </div>
+
+        <div className='article-tag-container'>
+          <span className='article-tag-span'>етикети</span>
+          {obj.tags.map((x) => (<Link className='article-tag-link' to={`/articles/tags`} state={{tag: `${x}`}}>{x}</Link>))}
         </div>
       </div>
     )
---
+++ src/components/Articles/Sections.js
@@ -0,0 +1,14 @@
+import './Sections.css'
+
+const Sections = (props) => {
+    
+    return (
+        <section className='post-section'>
+            <img className='post-image landscape' src={props.data.url}></img>
+            <p className='post-caption'>{props.data.caption}</p>
+            <div>{props.data.content.map(paragraph => (<p className='post-paragraph'>{paragraph}</p>))}</div>
+      </section>
+     );
+};
+
+export default Sections;
